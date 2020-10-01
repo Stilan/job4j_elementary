cc29f948872a8f572406919c36bfde7041d68ab3
Stilan <40999153+Stilan@users.noreply.github.com>
2020-10-01 15:09:32 +0700

    9.1. String [#374611]

---
+++ src/ru/job4j/calculator/Greeting.java
@@ -0,0 +1,12 @@
+package ru.job4j.calculator;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like java!";
+        System.out.println(idea);
+        idea += " But l am a newbie.";
+        int year = 2020;
+        idea += year;
+        System.out.println(idea);
+    }
+}
