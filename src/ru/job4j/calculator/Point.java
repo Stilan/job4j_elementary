9ea9a35fa31e47c34826e1871639029b43139ab6
Stilan <40999153+Stilan@users.noreply.github.com>
2020-10-02 17:31:18 +0700

    3. Тесты для определения расстояния между точками. [#374635]

--- src/ru/job4j/calculator/Point.java
+++ src/ru/job4j/calculator/Point.java
@@ -11,5 +11,8 @@ public class Point {
         double result2 = Point.distance(1, 3, -1, -9);
         double result3 = Point.distance(3, 5, -2, 0);
         System.out.println("result (0, 0) to (2, 0) " + result);
+        System.out.println("result (1, 3) to (-1, -9) " +  result2);
+        System.out.println("result (3, 5) to (-2, 0) " +  result3);
+
     }
 }
---
+++ test/ru/job4j/calculator/PointTest.java
@@ -0,0 +1,37 @@
+package ru.job4j.calculator;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PointTest {
+
+    @Test
+    public void distance() {
+        double expected = 2;
+        int x1 = 0;
+        int y1 = 0;
+        int x2 = 2;
+        int y2 = 0;
+        double out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+
+        expected = 12.16;
+        x1 = 1;
+        y1 = 3;
+        x2 = -1;
+        y2 = -9;
+        out = out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+
+        expected = 7.07;
+        x1 = 3;
+        y1 = 5;
+        x2 = -2;
+        y2 = 0;
+        out = out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+}
