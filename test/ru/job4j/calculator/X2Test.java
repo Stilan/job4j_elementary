d79c826bb47d16429fa5da575a8dde1cf250ce30
Stilan <40999153+Stilan@users.noreply.github.com>
2020-10-02 16:40:12 +0700

    1.1. Входные данные в тесте [#374638]

---
+++ test/ru/job4j/calculator/X2Test.java
@@ -0,0 +1,57 @@
+package ru.job4j.calculator;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class X2Test {
+
+    @Test
+    public void calc() {
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 1;
+
+        int expected = 3;
+
+        int rsl = X2.calc(a,b,c,x);
+
+        Assert.assertEquals(expected, rsl);
+
+         a = 0;
+         b = 1;
+         c = 1;
+         x = 1;
+
+         expected= 2;
+
+         rsl = X2.calc(a,b,c,x);
+
+        Assert.assertEquals(expected, rsl);
+
+        a = 1;
+        b = 1;
+        c = 0;
+        x = 1;
+
+        expected= 2;
+
+        rsl = X2.calc(a,b,c,x);
+
+        Assert.assertEquals(expected, rsl);
+
+        a = 1;
+        b = 1;
+        c = 1;
+        x = 0;
+
+        expected= 1;
+
+        rsl = X2.calc(a,b,c,x);
+
+        Assert.assertEquals(expected, rsl);
+
+    }
+}
