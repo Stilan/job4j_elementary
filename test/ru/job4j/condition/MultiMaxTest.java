93e45d80bd3d5c0733045077281bbd83a18125c9
Stilan <40999153+Stilan@users.noreply.github.com>
2020-10-08 12:29:14 +0700

    4.4. Вложенный оператор if else [#374562]

---
+++ test/ru/job4j/condition/MultiMaxTest.java
@@ -0,0 +1,31 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class MultiMaxTest {
+
+    @Test
+    public void max() {
+
+            int result = MultiMax.max(1, 4, 2);
+            assertThat(result, is(4));
+
+    }
+    @Test
+    public void max2() {
+
+        int result = MultiMax.max(4, 1, 2);
+        assertThat(result, is(4));
+
+    }
+    @Test
+    public void max3() {
+
+        int result = MultiMax.max(1, 2, 4);
+        assertThat(result, is(4));
+
+    }
+}
