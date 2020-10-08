ee24967d18842d8c3517e6dbeb5e3e8349ec5431
Stilan <40999153+Stilan@users.noreply.github.com>
2020-10-08 19:01:08 +0700

    5.2. Создать программу, вычисляющую факториал. [#374666]

---
+++ test/ru/job4j/loop/FactorialTest.java
@@ -0,0 +1,20 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class FactorialTest {
+
+    @Test
+    public void calc() {
+        int rsl = Factorial.calc(5);
+        assertThat(rsl, is(120));
+    }
+    @Test
+    public void calc2() {
+        int rsl = Factorial.calc(4);
+        assertThat(rsl, is(24));
+    }
+}
