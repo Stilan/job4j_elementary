b971af41d06e60b14810ee465cf0b8d21c28e3a6
Stilan <40999153+Stilan@users.noreply.github.com>
2020-10-25 13:26:18 +0700

    2.1. Рефакторинг - Расстояние между точками. [#374756]

--- src/ru/job4j/calculator/Point.java
+++ src/ru/job4j/calculator/Point.java
@@ -1,19 +1,26 @@
 package ru.job4j.calculator;
 
-public class Point {
-    public static double distance(int x1, int y1, int x2, int y2) {
-        double rsl = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-        return rsl;
-    }
-    public static void main(String[] args) {
-        double result = Point.distance(0, 0, 2, 0);
-        double result2 = Point.distance(1, 3, -1, -9);
-        double result3 = Point.distance(3, 5, -2, 0);
-        double result4 = Point.distance(8, 8, -3, -9);
-        System.out.println("result (0, 0) to (2, 0) " + result);
-        System.out.println("result (1, 3) to (-1, -9) " +  result2);
-        System.out.println("result (3, 5) to (-2, 0) " +  result3);
-        System.out.println("result (8, 8) to (-3, -9) " +  result4);
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
 
+public class Point {
+    private int x;
+
+    private int y;
+
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
+    }
+
+    public double distance(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
+    }
+
+    public static void main(String[] args) {
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double dist = a.distance(b);
+        System.out.println(dist);
     }
 }
