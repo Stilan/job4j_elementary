f07af859339ce146e773e549e87682d3c4ec3a38
Stilan <40999153+Stilan@users.noreply.github.com>
2020-11-06 19:05:30 +0700

    2. Scanner и чтение числа из консоли. [#374710]

--- src/ru/job4j/io/Matches.java
+++ src/ru/job4j/io/Matches.java
@@ -7,37 +7,22 @@ public class Matches {
         int numberOfMatches = 11;
         Scanner scanner = new Scanner(System.in);
         boolean queue =  true;
+        String name;
          while (numberOfMatches>0) {
-             if (queue) {
-                 System.out.println("Ход игрока № 1");
+                 name = queue ? " № 1" : " № 2";
+                 System.out.println("Ход игрока " + name);
                  int matches = Integer.valueOf(scanner.nextLine());
-                  if (matches == 1 || matches == 2 || matches == 3) {
+                  if (matches >= 1 && matches <= 3) {
                       numberOfMatches -= matches;
                       System.out.println("Осталось " + numberOfMatches);
-                      queue = false;
-                  }else {
-                      System.out.println("Введите число от 1 до 3");
-                      queue = true;
-                  }
-
+                      queue = !queue;
              }else {
-                 System.out.println("Ход игрока № 2");
-                 int matches = Integer.valueOf(scanner.nextLine());
-                 if (matches == 1 || matches == 2 || matches == 3) {
-                     numberOfMatches -= matches;
-                     System.out.println("Осталось " + numberOfMatches);
-                     queue = true;
-                 }else {
-                     System.out.println("Введите число от 1 до 3");
-                     queue = false;
-                 }
-             }
+                      System.out.println("Введите число от 1 до 3");
+                      queue = queue;
+                  }
          }
-         if (!queue){
-             System.out.println("Победа первого");
-         }else {
-             System.out.println("Победа второго");
-         }
+        name = queue ? "Победа игрока № 2" : "Победа игрока № 1";
+        System.out.println(name);
 
     }
 }
