cc8cef502c315e10849c34b510ffdebc43c731f7
Stilan <40999153+Stilan@users.noreply.github.com>
2020-11-27 14:37:56 +0700

    0,5. И Архия исключений и множественный улов. [# 374732]

--- src/ru/job4j/ex/UserStore.java
+++ src/ru/job4j/ex/UserStore.java
@@ -6,9 +6,11 @@ public class UserStore {
         for (User name : users) {
             if (login.equals(name.getUsername())){
                 user = name;
-            }else {
-                throw new UserNotFoundException();
+                break;
             }
+        }
+        if (user == null){
+            throw new UserNotFoundException();
         }
         return user;
     }
