8c69f2853c913ff930852496974e3c7e99fcdead
Stilan <40999153+Stilan@users.noreply.github.com>
2020-12-20 16:40:27 +0700

    1. Телефонный справочник на базе ArrayList [#374688]

---
+++ test/ru/job4j/collection/PhoneDictionaryTest.java
@@ -0,0 +1,21 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+
+public class PhoneDictionaryTest {
+
+    @Test
+    public void find() {
+            PhoneDictionary phones = new PhoneDictionary();
+            phones.add(
+                    new Person("Petr", "Arsentev", "534872", "Bryansk")
+            );
+            ArrayList<Person> persons = phones.find("72");
+            assertThat(persons.get(0).getSurname(), is("Arsentev"));
+        }
+}
