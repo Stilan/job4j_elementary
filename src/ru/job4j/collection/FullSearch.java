46575c142e8cc1939356c65064a3c910b794f60f
Stilan <40999153+Stilan@users.noreply.github.com>
2020-12-29 21:26:07 +0700

    1. Уникальные задачи. [# 374701]

---
+++ src/ru/job4j/collection/FullSearch.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class FullSearch {
+    public static HashSet<String> extractNumber(List<Task2> list) {
+        HashSet<String> numbers = new HashSet<>();
+        // for-each
+        for (Task2 task2: list) {
+           numbers.add(task2.getNumber());
+        }
+        return numbers;
+    }
+}
---
+++ src/ru/job4j/collection/Task2.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection;
+
+public class Task2 {
+    private String number;
+    private String description;
+
+    public Task2(String number, String description) {
+        this.number = number;
+        this.description = description;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+}
