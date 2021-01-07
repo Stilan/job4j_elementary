c25cead02e03542e66f73823a8fda0e2e80fb5b7
Stilan <40999153+Stilan@users.noreply.github.com>
2021-01-07 13:46:33 +0700

    3. Компаратор для строк [#374682]

--- src/ru/job4j/collection/StringCompare.java
+++ src/ru/job4j/collection/StringCompare.java
@@ -8,18 +8,15 @@ import static org.junit.Assert.assertThat;
 public class StringCompare implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-       int res = Integer.compare(left.length(),right.length());
-       if (res > 0){
-           right +=" ";
-       }else if (res < 0){
-           left +=" ";
-       }
-           for (int i = 0; i < left.length();i++){
+       int res = 0;
+       int minLength = Math.min(left.length(), right.length());
+           for (int i = 0; i < minLength;i++){
                res = Character.compare(left.charAt(i),right.charAt(i));
                if (!(res==0)){
                    return res;
                }
            }
-       return res;
+        res = Integer.compare(left.length(),right.length());
+        return res;
     }
 }
